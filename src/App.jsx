b687e1008c308dc5ecb79d9a9e22b08edb3fe42a
Kian2002 <canadakurdistan@gmail.com>
2023-02-20 20:42:12 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h1>Bob</h1>
+      </div>
+      <div className="card">
+        <h1>James</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
